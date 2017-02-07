798c42af62cc74494f1c525b52de4ee61d026216
Shreya S. Acharekar <Shreya S. Acharekar>
2017-02-06 20:32:21 -0600

    AAAAAA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">EFS</div>
+                <div class="sub title">Laravel 5</div>
             </div>
         </div>
     </body>
